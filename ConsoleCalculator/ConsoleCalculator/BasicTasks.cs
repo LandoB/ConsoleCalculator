0297ab18ff1eb00ff080adfca39b5ce8814b86dc
Lando B <jrolandobernal@gmail.com>
2016-01-27 14:45:42 -0600

    Created BasicTasks and Stack classes

---
+++ ConsoleCalculator/ConsoleCalculator/BasicTasks.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+
+ 
+namespace ConsoleCalculator
+{   // not static because we're going to instantiate an instance of this in Evaluate 
+    public class BasicTasks
+    { 
+        public int firstNumb; 
+        public int secNumb; 
+        public char OurDelimeter; 
+        public string ifChar; 
+
+    } 
+} 
+
---
+++ ConsoleCalculator/ConsoleCalculator/Stack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Stack
+    {
+        private static int lastanswer;
+
+        public static int LastAnswer
+        {
+            get { return lastanswer; }
+            set { lastanswer = value; }
+        }
+
+
+    }
+}
