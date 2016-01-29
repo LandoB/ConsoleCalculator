f7ea3fe80bc3c998159c9d133e42a05024a15077
Lando B <jrolandobernal@gmail.com>
2016-01-29 15:20:47 -0600

    Added Addtion and Substraction Unit Tests. All pass.

--- ConsoleCalculator/ConsoleCalculatorTest/CalculatorTests.cs
+++ ConsoleCalculator/ConsoleCalculatorTest/CalculatorTests.cs
@@ -13,5 +13,11 @@ namespace ConsoleCalculatorTest
         {
             Assert.AreEqual(Calculation.Addition(3, 2), 5);
         }
+
+        [TestMethod]
+        public void TestEnsureICanSubstractTwoNumbers()
+        {
+            Assert.AreEqual(Calculation.Substraction(3, 2), 1);
+        }
     }
 }
