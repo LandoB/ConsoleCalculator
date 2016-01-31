4eb3a739dbfc9e6dfd0e34cf59019298fb0aa4c6
Lando B <jrolandobernal@gmail.com>
2016-01-31 10:16:25 -0600

    Added code to Program. Basic calculations working. Need to work on handling exceptions now.

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -10,6 +10,76 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            var propeller = true;
+            BasicTasks myOperation = new BasicTasks();
+
+
+            while (propeller)
+            {
+
+                string prompt = "[" + counter + "]" + "> ";
+
+                // Saving user's input expression into a variable
+                Console.Write(prompt);
+                string theExpression = Console.ReadLine();
+
+                switch (theExpression)
+                {
+                    // Keyword for exiting application
+                    case "quit":
+                        Console.WriteLine("Bye!");
+                        Environment.Exit(1);
+                        return;
+
+                    // Keyword for exiting the application
+                    case "exit":
+                        Console.WriteLine("Bye!");
+                        Environment.Exit(1);
+                        return;
+
+                    // User entered "last" command
+                    case "last":
+                        Console.WriteLine(Stack.LastAnswer);
+                        break;
+
+                    // User entered "lastq" command
+                    case "lastq":
+                        Console.WriteLine(Stack.LastCommand);
+                        break;
+
+                    // User entered an expression to be evaluated
+                    default:
+
+                        // Stashing this away so it's ready if user enters "lastq" command
+                        if (theExpression != "lastq" || theExpression != "last")
+                        {
+                            Stack.LastCommand = theExpression;
+                        }
+
+                        // Finding delimeter
+                        myOperation.IdentifyOperator(theExpression);
+                        Evaluate evaluating = new Evaluate(myOperation);
+
+                        // Determine if we need to send this on for calculation or constant setting
+                        switch (myOperation.myDelimeter)
+                        {
+                            // Will get set as a constant
+                            case '=':
+                                myOperation.calcStack.SetTheConstant(myOperation.ifItIsChar, myOperation.secondNumber);
+                                break;
+
+                            default:
+                                evaluating.EvaluateThis(theExpression);
+
+                                // Increment counter with each round
+                                counter = counter + 1;
+                                Console.WriteLine("= " + Calculation.Answer);
+                                break;
+                        }
+                        break;
+                }
+            }
         }
     }
 }
