3e72f0032d9e1dcaee1eadb73747cede52682ef6
Lando B <jrolandobernal@gmail.com>
2016-01-30 22:47:26 -0600

    Added calculate operations to Evaluate class

--- ConsoleCalculator/ConsoleCalculator/Evaluate.cs
+++ ConsoleCalculator/ConsoleCalculator/Evaluate.cs
@@ -8,5 +8,38 @@ namespace ConsoleCalculator
 {
     class Evaluate
     {
+        public BasicTasks calculate { get; set; }
+
+        public Evaluate()
+        {
+            calculate = new BasicTasks();
+        }
+
+        public Evaluate(BasicTasks _task)
+        {
+            calculate = _task;
+        }
+
+        public int EvaluateThis(string expression)
+        {
+            calculate.IdentifyOperator(expression);
+
+
+            switch (calculate.myDelimeter)
+            {
+                case '+':
+                    return Calculation.Addition(calculate.firstNumber, calculate.secondNumber);
+                case '-':
+                    return Calculation.Substraction(calculate.firstNumber, calculate.secondNumber);
+                case '*':
+                    return Calculation.Multiplication(calculate.firstNumber, calculate.secondNumber);
+                case '/':
+                    return Calculation.Division(calculate.firstNumber, calculate.secondNumber);
+                case '%':
+                    return Calculation.Modulus(calculate.firstNumber, calculate.secondNumber);
+                default:
+                    throw new ArgumentException();
+            }
+        }
     }
 }
