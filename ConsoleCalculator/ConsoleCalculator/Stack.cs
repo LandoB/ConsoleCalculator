a7ed2aed680f80356639d4dca458294d99fbb82d
Lando B <jrolandobernal@gmail.com>
2016-01-30 22:16:15 -0600

    Added Dicitionary to Stack class so I can store and retrieve myConstants

--- ConsoleCalculator/ConsoleCalculator/Stack.cs
+++ ConsoleCalculator/ConsoleCalculator/Stack.cs
@@ -16,7 +16,39 @@ namespace ConsoleCalculator
             set { lastanswer = value; }
         }
 
+        private static string lastCommand = "My Command";
 
+        public static string LastCommand
+        {
+            get { return lastCommand; }
+
+            set { lastCommand = value; }
+        }
+
+        public Dictionary<string, string> ConstantHolder = new Dictionary<string, string>();
+
+        public void SetTheConstant(string firstNumber, int secondNumber)
+        {
+            string secondString = secondNumber.ToString();
+            ConstantHolder.Add(firstNumber, secondString);
+            return;
+        }
+
+        public static string WeFoundValue;
+        public string ValNotFound = "Value not found";
+
+        // Retrieving value from dictionary
+        public string RetrievingConstant(string firstNumber)
+        {
+            if (ConstantHolder.TryGetValue(firstNumber, out WeFoundValue))
+            {
+                return WeFoundValue;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
 
     }
 }
